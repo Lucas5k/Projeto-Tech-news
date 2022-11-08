2898825f628580f5e5281d2f7a018c6a8e552c56
Lucas5k <lucascabral112347@gmail.com>
2022-11-08 20:09:29 -0300

    feat: implementação da função next_page, concluida com sucesso

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,6 +31,9 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page = selector.css("div.nav-links > a.next::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
