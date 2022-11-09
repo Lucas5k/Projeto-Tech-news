2844f2900d1073a20c900f33f35a7ef9fe400c3e
Lucas5k <lucascabral112347@gmail.com>
2022-11-09 16:13:55 -0300

    feat: implementação do requisito scrape_noticia, conluido com sucesso

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -39,6 +39,34 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css('link[rel=canonical]::attr(href)').get()
+    title = selector.css(
+        'div.entry-header-inner h1.entry-title::text'
+    ).get().strip()
+    timestamp = selector.css('div.entry-header-inner li.meta-date::text').get()
+    writer = selector.css('div.entry-header-inner a.url::text').get()
+    comments_count = len(
+        selector.css('ol.comment-list div.comment-content::text').getall()
+    )
+    summary = "".join(
+        selector.css(
+            'div.entry-content > p:nth-of-type(1) *::text'
+        ).getall()).strip()
+    tags = selector.css('section.post-tags a[rel=tag]::text').getall()
+    category = selector.css('div.meta-category span.label::text').get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5
