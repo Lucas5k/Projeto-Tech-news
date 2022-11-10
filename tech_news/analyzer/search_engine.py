997c0793a7bdcd546741a3d0da3640010ef27d85
Lucas5k <lucascabral112347@gmail.com>
2022-11-10 19:26:30 -0300

    feat: implementação da função que busca pelo title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+from tech_news.database import search_news, find_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    my_tuple = tuple()
+    my_list_of_tuples = []
+    list_of_titles = search_news({"title": {"$regex": title, "$options": "i"}})
+    for titles in list_of_titles:
+        title = titles["title"]
+        url = titles["url"]
+        my_tuple = (title, url)
+        my_list_of_tuples.append(my_tuple)
+    return my_list_of_tuples
 
 
 # Requisito 7
